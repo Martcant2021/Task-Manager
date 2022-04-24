49520a7d8bfeef7567df886601c3c9b4a377de62
Martcant2021 <martincantero31@gmail.com>
2022-04-24 19:33:27 -0300

    crud & models

--- app/models.py
+++ app/models.py
@@ -1,2 +1,13 @@
-from sqlalchemy import Column, String, Integer
-from config import Base
+from turtle import title
+from xmlrpc.client import Boolean
+from sqlalchemy import Column, String, Integer, Boolean
+from config import db
+from app import app
+
+class Task(db):
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    description = Column(String)
+    status = Column(Boolean)
+
+
--- main.py
+++ main.py
@@ -4,6 +4,5 @@ import os
 IS_DEV = app.env == 'Entorno'
 
 if __name__=='__main__':
-    assert os.path.exists('.env')
     os.environ['FLASK_ENV'] = 'Entorno'
     app.run(debug=True)
